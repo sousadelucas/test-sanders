d2de5addba70b028637ba37efac60409a519d5c8
sousadelucas <lucasantos1bio@gmail.com>
2021-03-19 12:33:24 -0300

    responsividade

--- js/main.js
+++ js/main.js
@@ -9,23 +9,3 @@ new SimpleSlide ({
     time: 50000000,
     nav: true
 })
-
-
-function initAccordionList () {
-    const accordionList = document.querySelectorAll('.js-accordion .div ul h4');
-    if(accordionList.length) {
-    accordionList[0].classList.add('ativo');
-    accordionList[0].nextElementSibling.classList.add('ativo');
-
-    function activeAccordion() {
-        this.classList.toggle('ativo')
-        this.nextElementSibling.classList.toggle('ativo')
-};
-
-    accordionList.forEach((item) => {
-    item.addEventListener('click', activeAccordion)
-        });
-    }
-}
-
-initAccordionList ()
